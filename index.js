087111ac2144d70e131bbffd489ee7e317f54c96
japcdev <japc.dev@gmail.com>
2021-02-04 17:40:33 +0100

    Layout & ToDo at the menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { inquirerMenu, pause } = require('./helpers/inquirer');
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const Searches = require('./models/Searches');
 
 require('colors');
 
@@ -7,8 +8,40 @@ console.clear();
 const main = async ()=> {
     let opt;
 
+    const searches = new Searches();
+
     do {
         opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                // TODO Show message
+                const place = await readInput('City: ');
+                console.log(place);
+
+                // TODO Search the places
+
+                // TODO Select one place
+
+                // TODO Get data weather of the place
+
+                // TODO Show weather result of the place
+                console.log('\nCity details: \n'.green);
+                console.log('City: ', );
+                console.log('Lat: ', );
+                console.log('Lng: ', );
+                console.log('Min: ', );
+                console.log('Max: ', );
+                break;
+
+            case 2:
+                // Show the history of search
+
+                break;
+
+            default:
+                break;
+        }
 
         if (opt !== 0) await pause();
 
